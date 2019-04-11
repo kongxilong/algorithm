eefaa02306a0c204b917b21844e04f24647b82e1
kongxilong <kongxilong@fabu.ai>
2019-04-11 10:26:51 +0800

    add sort list solution

---
+++ leetcode/sort_linked_list/solution.h
@@ -0,0 +1,18 @@
+#include <vector>
+
+namespace sort_linked_list {
+  using namespace std;
+struct Point {
+  int x;
+  int y;
+  Point() : x(0), y(0) {}
+  Point(int a, int b) : x(a), y(b) {}
+};
+
+class Solution {
+public:
+  int maxPoints(vector<Point> &points);
+};
+
+} // namespace max_points
+
---
+++ leetcode/sort_linked_list/solution_test.cc
@@ -0,0 +1,19 @@
+#include "leetcode/max_points_on_a_line/solution.h"
+
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <vector>
+#include "gtest/gtest.h"
+
+namespace max_points {
+
+TEST(SolutionTest, SimpleTest) {
+  std::vector<Point> case0{Point(0, 0), Point(0, 0),Point{1,1}};
+  Solution solution;
+  EXPECT_EQ(solution.maxPoints(case0),3);
+  std::vector<Point> case1{Point(0, 0), Point(0, 2),Point{1,1}};
+  EXPECT_EQ(solution.maxPoints(case1),2);
+}
+
+}  // namespace max_points
